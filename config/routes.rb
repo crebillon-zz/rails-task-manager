f7a65226f758046b3c85852352dc749cb171624e
Charlotte Rebillon <charlotte_rebillon@orange.fr>
2019-02-12 14:52:58 +0100

    efficient to do list

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # READ ALL TASKS
+  get 'tasks', to: 'tasks#index'
+
+  # CREATE
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
+  # READ ONE TASK
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # UPDATE
+  get "tasks/:id/edit", to: "tasks#edit", as: :task_edit
+  patch "tasks/:id", to: "tasks#update"
+
+  delete "tasks/:id", to: "tasks#destroy"
 end
